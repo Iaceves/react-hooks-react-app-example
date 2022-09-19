fcc2608c33c3f3190b6633203631238c21942b67
iaceves <acevesaceves97@gmail.com>
2022-09-19 18:50:01 -0400

    commit tree 5237e99a072602c4d9f94477656700b36e003ff1

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
   return (
@@ -8,6 +9,7 @@ function ExampleComponent() {
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
+      
       <p>Whoa!</p>
     </div>
   );
